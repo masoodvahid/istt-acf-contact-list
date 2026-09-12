3e2c5cd926bfa31d5628050c5911b9991fc86cdd
Masood <masood.vahid@gmail.com>
2026-09-12 22:00:30 +0330

    Add custom CSS settings in v1.0.7

--- istt-acf-contact-list.php
+++ istt-acf-contact-list.php
@@ -3,7 +3,7 @@
  * Plugin Name: ISTT ACF Contact List
  * Plugin URI: https://github.com/masoodvahid/istt-acf-contact-list
  * Description: AJAX contact directory for the contacts category, powered by ACF fields.
- * Version: 1.0.6
+ * Version: 1.0.7
  * Requires at least: 6.0
  * Requires PHP: 7.4
  * Author: Masood Vahid
@@ -17,13 +17,14 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-define('ISTT_ACF_CONTACT_LIST_VERSION', '1.0.6');
+define('ISTT_ACF_CONTACT_LIST_VERSION', '1.0.7');
 define('ISTT_ACF_CONTACT_LIST_FILE', __FILE__);
 define('ISTT_ACF_CONTACT_LIST_BASENAME', plugin_basename(__FILE__));
 define('ISTT_ACF_CONTACT_LIST_DIR', plugin_dir_path(__FILE__));
 define('ISTT_ACF_CONTACT_LIST_URL', plugin_dir_url(__FILE__));
 
 require_once ISTT_ACF_CONTACT_LIST_DIR . 'includes/core.php';
+require_once ISTT_ACF_CONTACT_LIST_DIR . 'includes/settings.php';
 require_once ISTT_ACF_CONTACT_LIST_DIR . 'includes/github-updater.php';
 
 ISTT_ACF_Contact_List_GitHub_Updater::init();
